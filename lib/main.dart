41c5f4b5a300985d79a3f402228d11e8375a26ae
emirkorkmazz <emirkorkmaz000@gmail.com>
2024-10-05 13:38:58 +0300

    DI yapılandırması GetIt ve Injectable kullanılarak yapıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:btk_todo/app.dart';
 import 'package:flutter/material.dart';
 
+import '/core/core.dart';
+
 void main() {
+  /// Initialize [Service Locator]
+  configureDependencies();
+
   runApp(
     const App(),
   );
