40a24b36144a8a32577a64ac29bde80efb8d4c96
emirkorkmazz <emirkorkmaz000@gmail.com>
2024-10-05 10:18:32 +0300

    test silindi

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:btk_todo/home_view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
