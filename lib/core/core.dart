3510a88f8424efb50ed4ff2cba8704e258fbdaef
emirkorkmazz <emirkorkmaz000@gmail.com>
2024-10-06 13:39:15 +0300

    Environment Variables için Envied Eklendi

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,6 +1,7 @@
 export 'constants/constants.dart';
 export 'di/di.dart';
 export 'enums/enums.dart';
+export 'env/env.dart';
 export 'errors/errors.dart';
 export 'extension/extension.dart';
 export 'form_inputs/form_inputs.dart';
--- lib/core/di/register_module.dart
+++ lib/core/di/register_module.dart
@@ -4,24 +4,36 @@ import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '/data/data.dart';
 import '/core/core.dart';
+import '/data/data.dart';
 
 @module
 abstract class RegisterModule {
   ///
   @singleton
-  Dio get dio => Dio()..interceptors.add(PrettyDioLogger());
+  Dio get dio => Dio(
+        BaseOptions(
+          baseUrl: EnvConf.baseUrl,
+        ),
+      )..interceptors.add(
+          PrettyDioLogger(),
+        );
 
   ///
   @singleton
-  AuthClient get authClient => AuthClient(dio);
+  TodoClient get authClient => TodoClient(
+        dio
+          ..interceptors.add(
+            const TodoInterceptor(),
+          ),
+      );
 
   ///
   @singleton
-  TodoClient get todoClient =>
-      TodoClient(dio..interceptors.add(const TodoInterceptor()));
+  AuthClient get tokenClient =>
+      AuthClient(dio..interceptors.add(PrettyDioLogger()));
 
+  ///
   @singleton
   FlutterSecureStorage get securedStorage => const FlutterSecureStorage();
 
