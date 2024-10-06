37e79b73e6e818e5b1f77b59b32fe36e7c7fe08c
emirkorkmazz <emirkorkmaz000@gmail.com>
2024-10-06 11:51:35 +0300

    bug fix

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '/core/core.dart';
+import 'auth/auth.dart';
+import 'auth/login/bloc/login_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      /// [Router]
-      routerConfig: appRouter,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LoginBloc>(
+          create: (context) => getIt<LoginBloc>(),
+        ),
+        BlocProvider<SignupBloc>(
+          create: (context) => getIt<SignupBloc>(),
+        )
+      ],
+      child: MaterialApp.router(
+        /// [Router]
+        routerConfig: appRouter,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
 
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
+        /// [L10n]
+        /// Delegate: Temsilci Listesi
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
 
-      /// [L10n]
-      /// Delegate: Temsilci Listesi
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-
-      /// Desteklenen Diller
-      supportedLocales: AppLocalizations.supportedLocales,
+        /// Desteklenen Diller
+        supportedLocales: AppLocalizations.supportedLocales,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:btk_todo/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '/core/core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   /// Initialize [Service Locator]
   await configureDependencies();
 
